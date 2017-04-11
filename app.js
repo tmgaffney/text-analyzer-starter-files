2ce55b273991ec5a4b1c2662f974ac89fbc6b2ef
Theresa Gaffney <tmgaffney@gmail.com>
2017-04-10 23:44:09 -0400

    added rough ideas for js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// your code here!
+//handle the form submit?
+
+// convert submitted text to tokens
+	//sample string
+var rawString = "This is a short text about stuff."
+
+var tokens = rawString.toLowerCase().split(/\W+/);
+
+// total word count
+var totalWordCount = tokens.length;
+
+// unique word count
+function countUniqueWords(tokens) {
+	var uniqueWords = [];
+	for (var i=0; i < tokens.length; i++) {
+		if (uniqueWords.indexOf(tokens[i]) === -1) {
+			uniqueWords.push(tokens[i]);
+		}
+	}
+	return uniqueWords.length;
+}
+
+// average word length
+function getAverageLength(tokens) {
+	var totalLength = tokens.join("").length;
+	return (totalLength / totalWordCount);
+}
+
+
+// send results to page
